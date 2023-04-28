ac8c23d2986acb201b49b5643126ef3b3fc074f3
minhkhuy <khuy.tran@roboworld.io>
2023-04-28 13:10:43 +0700

    feat: add click outside for dropdown

---
+++ src/hooks/useOutsideAlerter.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useRef } from 'react';
+
+/**
+ * Hook that alerts clicks outside of the passed ref
+ */
+function useOutsideAlerter(ref: any, action: any) {
+  useEffect(() => {
+    /**
+     * Alert if clicked on outside of element
+     */
+    function handleClickOutside(event: any) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        action();
+      }
+    }
+    // Bind the event listener
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [ref]);
+}
+
+/**
+ * Component that alerts if you click outside of it
+ */
+export default function OutsideAlerter(props: any) {
+  const wrapperRef = useRef(null);
+  useOutsideAlerter(wrapperRef, props.action);
+
+  return <div ref={wrapperRef}>{props.children}</div>;
+}
